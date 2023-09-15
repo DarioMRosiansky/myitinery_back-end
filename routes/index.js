ab6dd7ba7a9c6c29584aed6cd87c7e9b253574ee
Dario Marcelo Rosiansky <100219404+DarioMRosiansky@users.noreply.github.com>
2023-09-15 14:38:35 -0300

    Login sprint 4 back end v5

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,12 @@ import express from "express";
 const router = express.Router();
 import citiesRouter from "./cities.js";
 import itinerariesRouter from "./itineraries.js";
+import usersRouter from "./users.js";
 
 
 router.use('/cities', citiesRouter);
 router.use('/itineraries',itinerariesRouter);
+router.use('/auth',usersRouter);
 
 
 
